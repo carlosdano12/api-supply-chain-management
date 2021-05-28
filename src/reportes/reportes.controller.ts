a401aa4c240aa26ed09d714b662c048e9e7c1b92
Carlos Salazar López <carlosdano12@gmail.com>
2021-05-28 00:11:53 -0500

    se añade  busqueda de solicitudes de compras

--- src/reportes/reportes.controller.ts
+++ src/reportes/reportes.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ReportesService } from './reportes.service';
 
+@ApiTags('Reportes')
 @Controller('reportes')
 export class ReportesController {
   constructor(private readonly _reportesService: ReportesService) {}
